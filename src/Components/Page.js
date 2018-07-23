f78377acdbcfd18c80da9cd6d0faff608e64df91
Matthew <matthewsun3@gmail.com>
2018-07-23 12:14:31 -0400

    merge conflicts

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -45,10 +45,6 @@ class Page extends Component {
 
         if (pageType === null) pageType = 'standard_page';
 
-<<<<<<< HEAD
-            let homepageDiv = <div className="homeImgDiv">{homeBgContents}</div>
-            homepageBgComponents.push(homeBgContents);
-=======
         //check if background image exists
         let pageStyle = {};
         if(document.data.background_image && document.data.background_image.url){
@@ -57,15 +53,11 @@ class Page extends Component {
                 backgroundSize: 'cover',
                 overflow: 'hidden'
             };
->>>>>>> 87e4532acafee72a50e161169221f36b8593d2fd
         }
 
         let sections = document.data.body;
-
-        let firstThreeComponents = [];
         let sectionsComponents = [];
 
-        let numComponents = 0;
         sections.forEach(element => {
 
             if (element.primary && element.primary.component_type) {
@@ -74,22 +66,6 @@ class Page extends Component {
                 let sectionContents = null;
 
                 if (sectionComponentType === 'header_section') {
-<<<<<<< HEAD
-                    sectionContents = <PageHeaderSection slice={element} pageType={pageType}/>;
-                    numComponents ++;
-                } else if (sectionComponentType === 'paragraph_section') {
-                    sectionContents = <PageParagraphSection slice={element} pageType={pageType}/>;
-                    numComponents ++;
-                } else if (sectionComponentType === 'image_card_section') {
-                    sectionContents = <PageImageCardSection slice={element} pageType={pageType}/>;
-                    numComponents ++;
-                } else if (sectionComponentType === 'image_section') {
-                    sectionContents = <PageImageSection slice={element} pageType={pageType}/>;
-                    numComponents ++;
-                } else if (sectionComponentType === 'blog_section') {
-                    sectionContents = <PageBlogSection slice={element} pageType={pageType}/>;
-                    numComponents ++;
-=======
                     sectionContents = <PageHeaderSection slice={element} pageType={pageType} />;
                 } else if (sectionComponentType === 'paragraph_section') {
                     sectionContents = <PageParagraphSection slice={element} pageType={pageType} />;
@@ -99,34 +75,17 @@ class Page extends Component {
                     sectionContents = <PageImageSection slice={element} pageType={pageType} />;
                 } else if (sectionComponentType === 'blog_section') {
                     sectionContents = <PageBlogSection slice={element} pageType={pageType} />;
->>>>>>> 87e4532acafee72a50e161169221f36b8593d2fd
                 }
 
-                if (sectionContents && numComponents > 3) {
+                if (sectionContents) {
                     let sectionDiv = <div className="pageSection">{sectionContents}</div>;
                     sectionsComponents.push(sectionDiv);
-                } else {
-                    let sectionDiv = <div className="pageSection">{sectionContents}</div>;
-                    firstThreeComponents.push(sectionDiv);
                 }
 
             }
         });
 
         return (
-<<<<<<< HEAD
-            <div className={pageType}>
-                    {homepageBgComponents}
-                    <Container className={'pageSections_' + pageType}>
-                        <Helmet>
-                            <title>{document.data.title && document.data.title + ' - '}{PrismicConfig.siteTitle}</title>
-                        </Helmet>
-                        {firstThreeComponents}
-                        <div className={'content_' + pageType}>
-                            {sectionsComponents}
-                        </div>
-                    </Container>
-=======
             <div className={pageType} style={pageStyle}>
                 <Container className='pageSections'>
                     <Helmet>
@@ -136,7 +95,6 @@ class Page extends Component {
                         {sectionsComponents}
                     </div>
                 </Container>
->>>>>>> 87e4532acafee72a50e161169221f36b8593d2fd
             </div>
         );
     }
